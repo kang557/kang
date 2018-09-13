99d529823533a3fea6d7d918ee04fc50b1d40bfb
kang557 <19970823lkq>
2018-09-13 15:50:38 +0800

    add kkkk

--- src/main/java/kkDemo.java
+++ src/main/java/kkDemo.java
@@ -2,5 +2,6 @@ public class kkDemo {
     public static void main(String[] args) {
         System.out.println("kk");
         System.out.println("kkk");
+        System.out.println("kkkk");
     }
 }
